fb1ef3e7d0c5cfc392cb9315b380e8c09b6916e0
player54562 <98137588+player54562@users.noreply.github.com>
2022-04-30 18:41:17 +0800

    Create new for python.py

---
+++ new for python.py
@@ -0,0 +1,55 @@
+while (ture):
+  password='cjk17819383236'
+  key=input('请输入p@ssword:')
+  if key==paasword:
+    print('海底两万里')
+    print('进制换算')
+    input('请选择：')
+    if input='1':
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
+     if input='2':
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+  else:
+    print('密码不正确')
