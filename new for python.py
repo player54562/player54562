9b1bad3bb5befa133d86dd55865cdd765f7c01c1
player54562 <98137588+player54562@users.noreply.github.com>
2022-05-07 17:31:17 +0800

    Update new for python.py

--- new for python.py
+++ new for python.py
@@ -1,55 +1,81 @@
-while (ture):
-  password='cjk17819383236'
-  key=input('请输入p@ssword:')
-  if key==paasword:
+password='cjk17819383236'
+
+key==input('请输入p@ssword:')
+
+while if key==password:
+
+    import os
+
+    os.system('cls')
+
     print('海底两万里')
-    print('进制换算')
+
+    print('进制转换')
+
     input('请选择：')
-    if input='1':
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-    
-     if input='2':
+
+    if input=='1':
+
+        import os
+
+        os.system('cls')
+
+        import pyttsx3
+
+        engine=pyttsx3.init()
+
+        engine.getProperty('voice', 'zh') 
+
+        engine.say('    ')
+
+        engine.runAndWait()
+
+        input(' ')
+
+        import pyttsx3
+
+        engine=pyttsx3.init()
+
+        engine.getProperty('voice', 'zh') 
+
+        engine.say('    ')
+
+        engine.runAndWait()
+
+        import os
+
+        os.system('F:')
+
+        os.system('    ')
+
+        import pyttsx3
+
+        engine=pyttsx3.init()
+
+        engine.getProperty('voice', 'zh') 
+
+        engine.say('    ')
+
+        engine.runAndWait()
+
+    if input=='2':
+
         
+
         
+
         
+
         
+
         
+
         
+
         
+
         
+
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-  else:
-    print('密码不正确')
+
+print('p@ssword不正确'）
